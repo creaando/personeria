d1741943c00b204bba065c3c2c9fe4a624f1a396
ches2409 <andresg2409@gmail.com>
2020-05-03 17:10:20 -0500

    carga inicial de boiler plate

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+import navigationOffCanvas from './components/navigation_off_canvas'
+import transparentHeader from './components/transparent_header'
+import slid from './components/slid'
+
+
+navigationOffCanvas()
+transparentHeader()
+slid()
+
